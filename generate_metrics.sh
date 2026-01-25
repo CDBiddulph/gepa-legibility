c9b462859bb1986ceb7b31a967330d0be044b566
Caleb Biddulph <caleb.d.biddulph@gmail.com>
2026-01-24 23:43:24 -0800

    Remove unneeded verbalizes_prompt_* metrics for tinker logs

--- generate_metrics.sh
+++ generate_metrics.sh
@@ -46,8 +46,7 @@ EXPERIMENT_REQUIREMENTS = {
     'teacher_true': ('proxy_reward true_reward hacking_rate verbalizes_prompt_yes verbalizes_prompt_mistargeted verbalizes_prompt_unclear verbalizes_prompt_no verbalizes_yes verbalizes_mistargeted verbalizes_unclear verbalizes_no', False),
     'teacher_false': ('proxy_reward true_reward hacking_rate verbalizes_prompt_yes verbalizes_prompt_mistargeted verbalizes_prompt_unclear verbalizes_prompt_no verbalizes_yes verbalizes_mistargeted verbalizes_unclear verbalizes_no', False),
     'baseline_only': ('hacking_rate verbalizes_yes verbalizes_mistargeted verbalizes_unclear verbalizes_no', True),
-    # tinker needs verbalizes_prompt_* for verbalization_comparison plot
-    'tinker': ('proxy_reward hacking_rate verbalizes_yes verbalizes_mistargeted verbalizes_unclear verbalizes_no verbalizes_prompt_yes verbalizes_prompt_mistargeted verbalizes_prompt_unclear verbalizes_prompt_no', True),
+    'tinker': ('proxy_reward hacking_rate verbalizes_yes verbalizes_mistargeted verbalizes_unclear verbalizes_no', True),
     'tinker_ckpts': ('hacking_rate verbalizes_yes verbalizes_mistargeted verbalizes_unclear verbalizes_no', True),
 }
 
